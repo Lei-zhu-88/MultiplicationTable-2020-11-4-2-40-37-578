9f8960a5523e62b641ae810dd0e595cb77ecea5f
Lei ZHU <lzhu19@slb.com>
2020-11-17 21:09:24 +0800

    feat:test case for 2 to 5

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -13,7 +13,7 @@ export class MultiplicationTable {
     const expressions: Expression[][] = buildExpressions(start, end)
     return renderTable(expressions)
 
-    
+
   }
 }
 
@@ -23,7 +23,7 @@ function isValidRange(start: number, end: number): boolean {
 }
 
 function isNotValidScope(start: number, end: number): boolean {
-  return (start < 1 || start > 10 )||(end < 1 || end > 10)
+  return start < 1 || start > 10 || end < 1 || end > 10
 
 }
 
@@ -57,6 +57,25 @@ function buildExpressions(start: number, end: number): Expression[][] {
 
 function renderTable(expressions: Expression[][]): string {
 
-  return  expressions.map(row => row.map(({ factor1, factor2, product }) => `${factor1}*${factor2}=${product}`).join('  ')).join('\n')
+  // return expressions.map(row => row.map(({ factor1, factor2, product }) => `${factor1}*${factor2}=${product}`).join('  ')).join('\n')
+  var expressionsString: string[][];
+  expressionsString = expressions.map(row => row.map(({ factor1, factor2, product }) => `${factor1}*${factor2}=${product}`))
+
+  for (var i = expressionsString.length - 2; i >= 0; i--) {
+    for (var j = 0; j < expressionsString[i].length-1; j++) {
+      if (expressionsString[i][j].length < expressionsString[expressionsString.length - 1][j].length) {
+        for (var k = 0; k < expressionsString[expressionsString.length - 1][j].length - expressionsString[i][j].length; i++) {
+          expressionsString[i][j] += ' '
+          
+        }
+      }
+
+
+    }
+
+  }
+
+  return expressionsString.map(row=>row.join('  ')).join('\n')
+
 }
 
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -60,18 +60,18 @@ describe('Multiplication Table', () => {
 
 
 
-  // it('should render multiplication table of (2, 5)', () => {
-  //   // given
-  //   const table = new MultiplicationTable()
-  //   const start = 2
-  //   const end = 5
+  it('should render multiplication table of (2, 5)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 5
 
-  //   //when
-  //   const rendered = table.render(start, end)
+    //when
+    const rendered = table.render(start, end)
 
-  //   //then
-  //   expect(rendered).toBe('2*2=4\n2*3=6   3*3=9\n2*4=8   3*4=12  4*4=16\n2*5=10  3*5=15  4*5=20  5*5=25')
-  // })
+    //then
+    expect(rendered).toBe('2*2=4\n2*3=6   3*3=9\n2*4=8   3*4=12  4*4=16\n2*5=10  3*5=15  4*5=20  5*5=25')
+  })
   
 
 
