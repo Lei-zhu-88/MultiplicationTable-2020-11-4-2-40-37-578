19e450756e8e94fdb334759d00a8becb4085b8bd
Lei ZHU <lzhu19@slb.com>
2020-11-17 14:55:07 +0800

    fest:test case for range not valid

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,25 @@
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
+
+  private  isValidRange(start: number, end: number): boolean {
+    return start <= end
+       
+  }
+
+
+  public render(start: number, end: number): string {
+
+    if (!this.isValidRange(start, end)) {
+      return ''
+    }
+
+    // if( start<1 ||end >10 ){
+    //   return 'out of range!'
+
+    // }
     return '1*1=1'
   }
+
+
+
 }
+
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -13,4 +13,48 @@ describe('Multiplication Table', () => {
     //then
     expect(rendered).toBe('1*1=1')
   })
+
+  // it('should remind that the range is not valid', () => {
+  //   // given
+  //   const table = new MultiplicationTable()
+  //   const start = 0
+  //   const end = 10
+
+  //   //when
+  //   const rendered = table.render(start, end)
+
+  //   //then
+  //   expect(rendered).toBe('out of range!')
+  // })
+
+  it('should return an empty string', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 1
+
+    //when
+    const rendered = table.render(start, end)
+
+    //then
+    expect(rendered).toBe('')
+  })
+
+  // it('should render multiplication table of (9, 10)', () => {
+  //   // given
+  //   const table = new MultiplicationTable()
+  //   const start = 9
+  //   const end = 10
+
+  //   //when
+  //   const rendered = table.render(start, end)
+
+  //   //then
+  //   expect(rendered).toBe('9*9=81')
+  // })
+  
+
+
+
+
 })
